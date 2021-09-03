cf047fb40295bf1c6086ac8c4d0c5e7efb1e63c3
Mark Camball <mark.camball@gmail.com>
2021-09-03 02:31:54 -0400

    works

--- contracts/ValidationTest.sol
+++ contracts/ValidationTest.sol
@@ -4,27 +4,27 @@ pragma solidity 0.6.9;
 import "./ECDSA.sol";
 
 contract ValidationTest {
-    address accountManager;
-	constructor(address _accountManager) public {
-        accountManager = _accountManager;
+
+	function validateSig(bytes memory signature,uint a) public returns (address){
+        bytes32 eip712DomainHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name)"
+                ),
+                keccak256(bytes("Velox"))
+            )
+        );
+        bytes32 hashStruct = keccak256(
+            abi.encode(
+                keccak256("Strategy(uint a)"),
+                a
+            )
+        );
+        bytes32 hash = keccak256(abi.encodePacked("\x19\x01", eip712DomainHash, hashStruct));
+        address signer = ECDSA.recover(hash, signature);
+        return signer;
+
+        return address(this);
 	}
-
-    function getManager() public view returns (address) {
-        return accountManager;
-    }
-
-    function hashForSignature(address _owner, uint256 _earningsToDate, uint256 _nonce) public pure returns (bytes32) {
-        return keccak256(abi.encode(_owner, _earningsToDate, _nonce));
-    }
-
-	function validateSig(address _to, uint256 _earningsToDate, uint256 _nonce, bytes memory _signature) public {
-        address signer = ECDSA.recover(ECDSA.toEthSignedMessageHash(hashForSignature(_to, _earningsToDate, _nonce)), _signature);
-        require(signer == accountManager, "signer is not the account manager");
-	}
-    function validateSig2(address _to, uint256 _earningsToDate, uint256 _nonce, bytes memory _signature) public {
-        address signer = ECDSA.recover(hashForSignature(_to, _earningsToDate, _nonce), _signature);
-        require(signer == accountManager, "signer is not the account manager");
-    }
-
 
 }
--- test/test.js
+++ test/test.js
@@ -1,45 +1,33 @@
 const ValidationTest = artifacts.require("ValidationTest");
 const ethers = require("ethers")
-const web3EthAbi = require("web3-eth-abi")
 
-const web3 = require("web3-eth-personal")
 const url = "http://localhost:8545"
 const provider = new ethers.providers.JsonRpcProvider(url)
-const privKey = "0x01ebdc3f0abb32b6d7b7cb4720699961eb899d05c7081a22b0b05f1cbad7f965"
+const privKey = "0x943ec4900ce279913b30094fd24192bbc7420ad13d66c439ea07f5ae39b36e2f"
 const signer = new ethers.Wallet(privKey, provider)
+const domain = {
+  name:"Velox",
+}
+const types = {Strategy:[
+    {name:"a", type:"uint"}
+  ]}
+  const a = 1
+const strategy = {a}
+
+
 
 contract('test', (accounts) => {
   it('validate a signature', async () => {
-    const ValidationTestDeployed = await ValidationTest.deployed();
-    const ownerAddress = "0x3a0c5f83d9418199d7a85e02712ddada239be244"
-    const earningsToDate = "500"
-    const nonce = "100"
-
-
-
-    /** CHANGE STUFF HERE **/
-    const encoded =  ethers.utils.defaultAbiCoder.encode(
-      ["address", "uint256", "uint256"],
-      [ownerAddress, earningsToDate, nonce]
+    const signature = await signer._signTypedData(
+      domain,
+      types,
+      strategy
     )
-    const hash_old = ethers.utils.keccak256(encoded)
-
-    const hash = await ValidationTestDeployed.hashForSignature(ownerAddress, earningsToDate, nonce)
-    console.log(hash,"\n",hash_old )
-
-    let messageHashBytes = ethers.utils.arrayify(hash)
-    // const signature = await signer.signingKey.signDigest(hash)
-    const signature = await signer.signMessage(messageHashBytes)
-    // console.log(web3)
-    // const signature = web3.sign(hash, accounts[0], console.log)
-
-
-    console.log(signer.address)
-    console.log(await ValidationTestDeployed.getManager())
-
-
-    await ValidationTestDeployed.validateSig(ownerAddress, earningsToDate,nonce, signature);
-    // await ValidationTestDeployed.validateSig2(ownerAddress, earningsToDate,nonce, signature);
-
+    console.log("sig",signature)
+    const actualSigner = ethers.utils.verifyTypedData(domain,types,strategy, signature)
+    console.log("signer", actualSigner)
+    const ValidationTestDeployed = await ValidationTest.deployed();
+    const res = await ValidationTestDeployed.validateSig.call(signature,a);
+    console.log("res",res)
   });
 });
